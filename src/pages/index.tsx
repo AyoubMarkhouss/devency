e77c24ec3f058c281a73cb911cdb43a4e4d99d6e
AyoubMarkhouss <ayoub.markhouss@gmail.com>
2024-01-24 16:56:47 +0100

    meta tag

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -20,10 +20,11 @@ export default function Home() {
       <Head>
         <meta
           name="description"
-          content="Découvrez des possibilités illimitées avec l&apos;agence de marketing Devency, votre partenaire de confiance pour des solutions informatiques innovantes. De la développement d'applications et de sites web à la stratégie de marque et d&apos;identité, nous fusionnons créativité et précision. Choisissez Devency pour une collaboration brillante, une excellence en assurance qualité, et un engagement à façonner la technologie de demain dès aujourd&apos;hui. Élevez votre expérience numérique avec une équipe dévouée à votre succès."
+          content="Découvrez des possibilités illimitées avec l'agence de marketing Devency, votre partenaire de confiance pour des solutions informatiques innovantes. De la développement d'applications et de sites web à la stratégie de marque et d'identité, nous fusionnons créativité et précision. Choisissez Devency pour une collaboration brillante, une excellence en assurance qualité, et un engagement à façonner la technologie de demain dès aujourd'hui. Élevez votre expérience numérique avec une équipe dévouée à votre succès."
         />
         <link rel="icon" href="/devency.ico" />
         <title>Devency - Home</title>
+        <meta property="og:image" content="/dev3white.png" />
       </Head>
       <main className="min-h-[calc(100vh_-_90px)] bg-[#161A30]">
         <Homepage />
