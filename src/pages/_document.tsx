8d2b1ad0ad713a732f368e7622db400bb31452e5
AyoubMarkhouss <ayoub.markhouss@gmail.com>
2024-02-08 19:36:59 +0100

    analytics

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,13 +1,14 @@
-import { Html, Head, Main, NextScript } from 'next/document'
-
+import { Html, Head, Main, NextScript } from "next/document";
+import { Analytics } from "@vercel/analytics/react";
 
 export default function Document() {
   return (
-    <Html lang="en" >
+    <Html lang="en">
       <Head />
       <body className="dark text-foreground bg-[#161A30]">
         <Main />
         <NextScript />
+        <Analytics />
       </body>
     </Html>
   );
